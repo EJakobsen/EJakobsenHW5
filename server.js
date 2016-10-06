09ed6366aa445a328b68753ae35c0526dc69cf75
Emily Jakobsen <emily.jakobsen@yale.edu>
2016-10-06 02:15:01 +0000

    Add nickname super-scorpion.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+app.get('/nickname', function(request, response){
+    response.send('super-scorpion');
 });
-
+app.get('/', function(request, response){
+    response.send('Hello from the other side.');
+});
 app.listen(process.env.PORT || 4000);
